1925403db863c94e6007a68fae200be031433cc7
Marian <MarianCrofts@gmail.om>
2016-04-20 22:48:59 -0700

    Testing4

--- app/src/main/java/damson/suites/suites/LoginActivity.java
+++ app/src/main/java/damson/suites/suites/LoginActivity.java
@@ -34,7 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
-
+WHYYYYY
 /**
  * A login screen that offers login via email/password.
  */
