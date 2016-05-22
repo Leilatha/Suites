bffd2c963d68037f45c5bdb299617efa46e10d50
Marian <MarianCrofts@gmail.om>
2016-05-21 23:03:34 -0700

    Started working on the chores list

---
+++ app/src/main/java/damson/suites/suites/ChoresList.java
@@ -0,0 +1,13 @@
+package damson.suites.suites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoresList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chores_list);
+    }
+}
