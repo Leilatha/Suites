ed1ce32b1da3062de0cf9a7ea0cde32592fc670d
Michael Chin <mjsangjoon@hotmail.com>
2016-05-25 15:18:31 -0700

    Commit 15 Grocery converted to Fragment

--- app/src/main/java/damson/suites/suites/GroceryBasket.java
+++ app/src/main/java/damson/suites/suites/GroceryBasket.java
@@ -41,7 +41,7 @@ import android.support.design.widget.CoordinatorLayout;
 import com.roughike.bottombar.BottomBarFragment;
 import com.roughike.bottombar.OnMenuTabSelectedListener;
 
-public class GroceryBasket extends AppCompatActivity {
+public class GroceryBasket extends Fragment {
     static final int itemIdentifier = 1;  // The request code
     ArrayAdapter myAdapter;
 
@@ -53,30 +53,31 @@ public class GroceryBasket extends AppCompatActivity {
      * may be best to switch to a
      * {@link FragmentStatePagerAdapter}.
      */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    //private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_grocery_basket, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_grocery_basket);
+        //setContentView(R.layout.fragment_grocery_basket);
 
         //TODO: fix with database stuff
 
         listMaker();
-        BottomBar mBottomBar = BottomBar.attach(this, savedInstanceState);
-        mBottomBar.setFragmentItems(getSupportFragmentManager(), R.id.fragmentContainer,
-            new BottomBarFragment(PSA.newInstance(), R.drawable.psa, "PSA")
-        );
+        //BottomBar mBottomBar = BottomBar.attach(this, savedInstanceState);
+        //mBottomBar.setFragmentItems(getSupportFragmentManager(), R.id.fragmentContainer,
+            //new BottomBarFragment(PSA.newInstance(), R.drawable.psa, "PSA")
+        //);
 
         /* mBottomBar.setItemsFromMenu(R.menu.three_buttons_menu, new OnMenuTabSelectedListener() {
             @Override
@@ -106,7 +107,7 @@ public class GroceryBasket extends AppCompatActivity {
         } */
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        //mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         /*mViewPager = (ViewPager) findViewById(R.id.container);
@@ -122,7 +123,7 @@ public class GroceryBasket extends AppCompatActivity {
          * It receives new items from that activity, and then
          * displays it into the list.
          */
-        final Button addButton = (Button) findViewById(R.id.grocery_basket_add_button);
+        final Button addButton = (Button) getView().findViewById(R.id.grocery_basket_add_button);
         if(addButton == null){
             System.out.println("ERROR");
             return;
@@ -140,7 +141,7 @@ public class GroceryBasket extends AppCompatActivity {
      */
     public void buttonPress() {
         System.out.println("I MADE IT TO BUTTON PRESSSSSSSSSSSS");
-        Intent receiveItemIntent = new Intent(this, GroceryBasketAdd.class);
+        Intent receiveItemIntent = new Intent(getActivity(), GroceryBasketAdd.class);
         //setContentView(R.layout.activity_grocery_basket_add);
         startActivityForResult(receiveItemIntent, itemIdentifier);
     }
@@ -160,7 +161,7 @@ public class GroceryBasket extends AppCompatActivity {
         helper.listSuiteGroceries(Suite.suite.getId(), new AsyncResponseHandler<DBGroceryListResult>() {
             @Override
             public void onSuccess(DBGroceryListResult response, int statusCode, Header[] headers, byte[] errorResponse) {
-                ListView myList = (ListView) findViewById(R.id.grocery_basket_listView);
+                ListView myList = (ListView) getView().findViewById(R.id.grocery_basket_listView);
 
                 myList.setVisibility(View.VISIBLE);
 
@@ -169,7 +170,7 @@ public class GroceryBasket extends AppCompatActivity {
                     myAdapter = null;
                     if (myList != null)
                         myList.setVisibility(View.GONE);
-                    TextView tv = (TextView) findViewById(R.id.noItemsView);
+                    TextView tv = (TextView) getView().findViewById(R.id.noItemsView);
                     tv.setVisibility(View.VISIBLE);
                     System.out.println("NOTE: no items in myList");
                     return;
@@ -177,7 +178,7 @@ public class GroceryBasket extends AppCompatActivity {
 
                 // There are items
                 myAdapter = new GroceryAdapter(
-                        getApplicationContext(), (ArrayList<Grocery>) response.getGroceryList());
+                        getActivity(), (ArrayList<Grocery>) response.getGroceryList());
                 if (myList != null) {
                     myList.setVisibility(View.VISIBLE);
                     myList.setAdapter(myAdapter);
@@ -190,7 +191,7 @@ public class GroceryBasket extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                FrameLayout frame = (FrameLayout) findViewById(R.id.fragmentContainer);
+                FrameLayout frame = (FrameLayout) getView().findViewById(R.id.fragmentContainer);
                 Snackbar
                         .make(frame, R.string.error_network_connection, Snackbar.LENGTH_LONG)
                         .show();
@@ -200,17 +201,17 @@ public class GroceryBasket extends AppCompatActivity {
             @Override
             public void onLoginFailure(Header[] headers, byte[] errorResponse, Throwable e) {
                 // TODO: Add "please log in again" code
-                ListView myList = (ListView) findViewById(R.id.grocery_basket_listView);
+                ListView myList = (ListView) getView().findViewById(R.id.grocery_basket_listView);
                 if (myList != null)
                     myList.setVisibility(View.GONE);
-                TextView tv = (TextView) findViewById(R.id.noItemsView);
+                TextView tv = (TextView) getView().findViewById(R.id.noItemsView);
                 tv.setVisibility(View.VISIBLE);
                 System.out.println("ERROR: Not logged in.");
             }
 
             @Override
             public void onFinish(){
-                final ListView myList = (ListView) findViewById(R.id.grocery_basket_listView);
+                final ListView myList = (ListView) getView().findViewById(R.id.grocery_basket_listView);
 
                 myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
@@ -224,97 +225,11 @@ public class GroceryBasket extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onRestart() {
-        super.onRestart();
-        listMaker();
-    }
-
 
     @Override
     public void onStop() {
         super.onStop();
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_grocery_basket, container, false);
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
-
-    /**
-     * Lexie Rochfort
-     * 5/7/2016
-     */
-    private class dataHandler {
-        //pull info from database
-        //put into array of arrays
-        //make array of arrays into list items
-        //go through array and make  list item out of
-
-    }
 }
